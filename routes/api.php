674f1c6e35f42384d558846023bbc8ea33563f6b
Florent Cuer <florentcuer@gmail.com>
2024-10-01 13:08:43 +0100

    added formatting changes

--- routes/api.php
+++ routes/api.php
@@ -3,13 +3,12 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/timesheets', [\App\Http\Controllers\TimesheetController::class, 'addTimesheet']);
-
 Route::get('/timesheets', [\App\Http\Controllers\TimesheetController::class, 'getAllTimesheets']);
 Route::get('/timesheets/employee/{id}', [\App\Http\Controllers\TimesheetController::class, 'getTimesheetsByEmployeeId']);
 Route::get('/timesheets/project/{id}', [\App\Http\Controllers\TimesheetController::class, 'getTimesheetsByProjectId']);
-Route::delete('timesheets/{id}', [\App\Http\Controllers\TimesheetController::class, 'deleteTimesheetById']);
-Route::put('timesheets/{id}', [\App\Http\Controllers\TimesheetController::class, 'updateTimesheetById']);
+Route::post('/timesheets', [\App\Http\Controllers\TimesheetController::class, 'addTimesheet']);
+Route::put('/timesheets/{id}', [\App\Http\Controllers\TimesheetController::class, 'updateTimesheetById']);
+Route::delete('/timesheets/{id}', [\App\Http\Controllers\TimesheetController::class, 'deleteTimesheetById']);
 
 Route::get('/projects', [\App\Http\Controllers\ProjectController::class, 'getAllProjects']);
 Route::post('/projects', [\App\Http\Controllers\ProjectController::class, 'addProject']);
